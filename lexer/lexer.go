4159e93c8a88067433c8764609e64c803ff83662
RPiAwesomeness <keyboardmailtesting@gmail.com>
2017-08-14 22:32:48 -0400

    First test set complete!

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,8 @@
 package lexer
 
+import "github.com/RPiAwesomeness/in.flux/token"
+
+// Lexer keeps track of the information needed to lex out a given string
 type Lexer struct {
 	input        string
 	position     int  // Current position in input (points to current char)
@@ -7,7 +10,113 @@ type Lexer struct {
 	ch           byte // Current char under examination
 }
 
+// New takes an input string and creates a lexer based on that string
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+// TODO: Make this support full Unicode (emojis baby!) not just ASCII
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+// readNumber iterates over the current character for the lexer until it's no longer a number
+// then it returns the values from the start of the number to the end
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+// NextToken reads the current character for the lexer (skipping any whitespace first) and returns the
+// appropriate token, if one exists. If not, it will first attempt to find the appropriate identifier
+// then, failing that, return the ILLEGAL token
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRACKET, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		}
+
+		tok = newToken(token.ILLEGAL, l.ch)
+	}
+
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// skipWhitespace does exactly that. When called, it will iterate over the current character
+// for the lexer, incrementing it until the character is not a whitespace character
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// isLetter validates whether or not a character is a letter or the _ character, both of which
+// can be used for creating identifiers
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
--- token/token.go
+++ token/token.go
@@ -38,3 +38,18 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent checks the keywords map to see if there are any matching language reserved keywords that match
+// the supplied identifier
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
