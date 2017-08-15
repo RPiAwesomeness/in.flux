5918bc9efb1943678e5e4c6dcd813ed6c9a16ed6
RPiAwesomeness <keyboardmailtesting@gmail.com>
2017-08-14 23:52:02 -0400

    Added fancy message thingy to REPL screen Updated README Added current binary

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # in.flux
 in.Flux is a little interpreted programming language I created to learn how interpreted languages work
+
+Planned features:
+  * Builtin macros:
+    ```
+    let oneThroughFive = [1, 2, 3, 4, 5]
+    let data = {"spam": "eggs", "foo": 5}
+    // println prints strings, numbers, and raw versions of objects to stdout, followed by a newline character
+    println("Strings and numbers and objects can be printed, followed by a newline \n")
+    println(5)
+    println(oneThroughFive)
+    println(data)
+    ```
+
+  * Function chaining:
+    ```
+    let addStuff = fn(x, y) {
+      x + y;
+    };
+    let numberIsntFive = fn(x) {
+      x != 5
+    }
+    let wasSuccess = fn(res) {
+      if res == true {
+        println("Success!")
+      } else {
+        println("Failure :(")
+      }
+    }
+
+    // Will take the output from addStuff(5, 10) and feed it into numberIsntFive which feeds into wasSuccess
+    addStuff(5, 10)->numberIsntFive->wasSuccess
+    ```
--- main.go
+++ main.go
@@ -4,15 +4,53 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"strings"
 
 	"github.com/RPiAwesomeness/in.flux/repl"
 )
+
+const version = "v0.0.1 beta"
+
+var greeting = `
+╔══════════════════════════════════╗
+║   _           ___ _              ║
+║  (_)_ __     / __\ |_   ___  __  ║
+║  | | '_ \   / _\ | | | | \ \/ /  ║
+║  | | | | |_/ /   | | |_| |>  <   ║
+║  |_|_| |_(_)/    |_|\__,_/_/\_\  ║
+║                                  ║
+║%s║
+║%s║
+╚══════════════════════════════════╝
+`
 
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! Welcome to the in.Flux REPL!\n", user.Username)
+	fmt.Printf(greeting, center(34, version), center(34, "Welcome", user.Username))
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func center(length int, value ...string) string {
+	var lPad, rPad string
+	// Can't center if what we're supposed to be centering is longer than the available space
+	if len(value) > length {
+		return ""
+	}
+	var valLen = len(strings.Join(value, " "))
+	var available = length - valLen
+	if available%2 != 0 {
+		// it's an odd number, pad the left side more
+		lPad += " "
+		available--
+	}
+	available /= 2
+	for i := 0; i < available; i++ {
+		lPad += " "
+		rPad += " "
+	}
+
+	return fmt.Sprint(lPad + strings.Join(value, " ") + rPad)
 }
